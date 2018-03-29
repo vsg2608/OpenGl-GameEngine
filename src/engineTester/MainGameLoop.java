ecb4cceb3568308419dde9237f039166a1d0563b
vsg2608 <vsg2608@gmail.com>
2018-03-30 02:42:04 +0530

    Save-tutorial2

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,6 +3,9 @@
 import org.lwjgl.opengl.Display;
 
 import RenderEngine.DisplayManager;
+import RenderEngine.Loader;
+import RenderEngine.RawModel;
+import RenderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,14 +13,33 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		
+		Loader loader= new Loader();
+		Renderer renderer= new Renderer();
+		
+		//OpenGL expects vertices to be defined counter clockwise by default
+		 float[] vertices = {
+			    //First Triangle
+				-0.5f, 0.5f, 0f,
+			    -0.5f, -0.5f, 0f,
+			    0.5f, -0.5f, 0f,
+			    //Second triangle
+			    0.5f, -0.5f, 0f,
+			    0.5f, 0.5f, 0f,
+			    -0.5f, 0.5f, 0f
+			  };
+
+		RawModel model= loader.loadtoVAO(vertices);
+		
 		while(!Display.isCloseRequested()) {
+			renderer.prepare();
 			
 			//game logic
-			// Rendering
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 			
 		}
 		
+		loader.CLeamUp();
 		DisplayManager.closeDisplay();
 
 	}
