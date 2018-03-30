d261e8d378ae8e6afdd09e80abe286e4bbbf4a20
vsg2608 <vsg2608@gmail.com>
2018-03-30 14:15:42 +0530

    Save-tutorial6

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,6 +1,10 @@
 	package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
+	
+	private int location_transformationMatrix;
 
 	private static final String VERTEX_FILE="src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE="src/shaders/fragmentShader.txt";
@@ -15,4 +19,11 @@ public class StaticShader extends ShaderProgram{
 		super.bindAttribute(1, "textureCoords");
 	}
 
+	protected void getAllUniformLocations() {
+		location_transformationMatrix=super.getUniformLocation("transformationMatrix");
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
 }
